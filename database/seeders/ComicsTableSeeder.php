68f87f31e6bea5c4d2f77b9743c10b073aa90742
estherpinos <estherpinos1@gmail.com>
2023-11-26 16:21:04 +0100

    Add [seeder]

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $comics = config('comics');
+        dump($comics);
+    }
+}
