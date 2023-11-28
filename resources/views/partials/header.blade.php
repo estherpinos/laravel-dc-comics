3db17f4c1fd9dc24fbcda551ac2a19fef6d4acb3
estherpinos <estherpinos1@gmail.com>
2023-11-28 15:38:19 +0100

    Add [nav]

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+
+</header>
