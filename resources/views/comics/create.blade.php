02a42f94ae4b4719ed79e69178127296f847ca3d
estherpinos <estherpinos1@gmail.com>
2023-11-28 21:16:05 +0100

    Add [form ]

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,6 +3,40 @@
 @section('content')
 <h1>Inserimento nuovo comic</h1>
 
-<form action="{{route('comic.store')}}" method="POST"></form>
+<div class="row">
+    <div class="col-8">
+        <form action="{{route('comic.store')}}" method="POST"></form>
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <input type="text" class="form-control" id="description" name="description">
+            </div>
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Imagine</label>
+                <input type="text" class="form-control" id="thumb" name="thumb">
+            </div>
+            <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="text" class="form-control" id="price" name="price">
+            </div>
+            <div class="mb-3">
+                <label for="series" class="form-label">Series</label>
+                <input type="text" class="form-control" id="series" name="series">
+            </div>
+            <div class="mb-3">
+                <label for="date" class="form-label">Sales date</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date">
+            </div>
+            <div class="mb-3">
+                <label for="type" class="form-label">Type</label>
+                <input type="text" class="form-control" id="type" name="type">
+            </div>
+
+    </div>
+</div>
+
 
 @endsection
