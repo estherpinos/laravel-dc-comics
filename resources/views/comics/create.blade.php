f9071e4f103198cedb4a538da83636a9577a86b3
estherpinos <estherpinos1@gmail.com>
2023-11-28 22:03:03 +0100

    Add [store]

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,7 +5,11 @@
 
 <div class="row">
     <div class="col-8">
+
+        @csrf
         <form action="{{route('comic.store')}}" method="POST">
+            @csrf
+
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control" id="title" name="title">
